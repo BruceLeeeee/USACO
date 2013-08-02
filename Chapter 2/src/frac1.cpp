3c3e115ccb59cd6fc7efef5dcc3ccce009fda300
Yotta2 <cloudsweet1989@gmail.com>
2013-08-02 17:18:23 -0400

    modified frac1

--- Chapter 2/src/frac1.cpp
+++ Chapter 2/src/frac1.cpp
@@ -13,8 +13,6 @@ LANG: C++
 #include <algorithm>
 #include <cmath>
 
-#define SIZE 165
-
 using namespace std;
 
 struct fraction {
@@ -23,43 +21,28 @@ struct fraction {
 };
 
 int gcd(int a, int b) {
-    if (b == 0)
-        return a;
-    else
-        return gcd(b, a % b);
+    return b == 0 ? a : gcd(b, a % b);
 }
 
 bool cmp(const fraction &a, const fraction &b) {
-    if (a.num * b.den <= b.num * a.den)
-        return true;
-    else
-        return false;
+    return a.num * b.den <= b.num * a.den;
 }
 
-void solve(int n, bool flag[SIZE][SIZE], vector<fraction> &ans) {
+void solve(int n, vector<fraction> &ans) {
 
     for (int i = 1; i <= n; i++)
         for (int j = i; j <= n; j++) {
             int num = i;
             int den = j;
             int factor = gcd(num, den);
-            num /= factor;
-            den /= factor;
+            if (factor != 1)
+                continue;
             fraction frac;
             frac.num = num;
             frac.den = den;
-            if (flag[num][den] == false) {
-                ans.push_back(frac);
-                flag[num][den] = true;
-            }
+            ans.push_back(frac);
         }
     sort(ans.begin(), ans.end(), cmp);
-}
-
-void init(int n, bool flag[SIZE][SIZE]) {
-    for (int i = 0; i <= n; i++)
-        for (int j = 0; j <= n; j++)
-            flag[i][j] = false;
 }
 
 void output(vector<fraction> &ans, ofstream &fout) {
@@ -71,15 +54,13 @@ int main() {
     ofstream fout("frac1.out");
     ifstream fin("frac1.in");
     int n;
-    bool flag[SIZE][SIZE];
     vector<fraction> ans;
     fraction frac;
     frac.den = 1;
     frac.num = 0;
     ans.push_back(frac);
     fin >> n;
-    init(n, flag);
-    solve(n, flag, ans);
+    solve(n, ans);
     output(ans, fout);
     return 0;
 }
